abab7ed3565ea9f0cdffac21de6d21829da273ef
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2024-11-04 16:17:26 +0500

    refactor: add reusable swiper

--- src/app/screens/home-screen.tsx
+++ src/app/screens/home-screen.tsx
@@ -4,8 +4,37 @@ import { Layout } from "@app/layouts/layout";
 import { useNavigation } from "@react-navigation/native";
 import MyTouchableOpacity from "@shared/ui/MyTouchableOpacity/my-touchable-opacity";
 import LinkButton from "@shared/ui/LinkButton/link-button";
-import ClubsSwiper from "@features/ui/ClubSwiper/club-swiper";
 import { BarSwiper } from "@features/ui/BarSwiper/bar-swiper";
+import ReusableSwiper from "@features/ui/Swiper/swiper";
+import { ClubCard } from "@features/ui/ClubCard/club-card";
+
+const clubsContent = [
+  {
+    clubName: "PC Club Name",
+    clubLocation: "Улица Жандосова, 58 / 1",
+    distance: 4,
+  },
+  {
+    clubName: "PC Club Name",
+    clubLocation: "Улица Жандосова, 58 / 1",
+    distance: 4,
+  },
+  {
+    clubName: "PC Club Name",
+    clubLocation: "Улица Жандосова, 58 / 1",
+    distance: 4,
+  },
+  {
+    clubName: "PC Club Name",
+    clubLocation: "Улица Жандосова, 58 / 1",
+    distance: 4,
+  },
+  {
+    clubName: "PC Club Name",
+    clubLocation: "Улица Жандосова, 58 / 1",
+    distance: 4,
+  },
+];
 
 export const HomeScreen = () => {
   return (
@@ -18,7 +47,20 @@ export const HomeScreen = () => {
           <LinkButton text="Смотреть все" screen="OurClubs" />
         </View>
       </View>
-      <ClubsSwiper />
+      <ReusableSwiper
+        data={clubsContent}
+        loop={false}
+        autoplay={false}
+        renderItem={(item) => (
+          <ClubCard
+            clubName={item.clubName}
+            clubLocation={item.clubLocation}
+            distance={item.distance}
+          />
+        )}
+        height={273}
+        gap={8}
+      />
       <View className="mt-8">
         <View className="flex flex-row items-center justify-between">
           <Text weight="bold" className="text-light text-2xl">
