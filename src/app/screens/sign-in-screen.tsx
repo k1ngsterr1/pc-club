b37e99766b81d15cae9e6c3fe6f85153e2da4c51
fwefew123 <artem.andreev200012@mail.ru>
2024-11-29 14:39:18 +0500

    feat: sign-in

--- src/app/screens/sign-in-screen.tsx
+++ src/app/screens/sign-in-screen.tsx
@@ -12,34 +12,34 @@ export const RegistrationScreen = () => {
   const navigation = useNavigation();
   const { i18n } = useLanguage();
 
-  const handleSendCode = async () => {
-    if (!phoneNumber) {
-      Alert.alert("Ошибка", "Введите номер телефона.");
-      return;
-    }
+  // const handleSendCode = async () => {
+  //   if (!phoneNumber) {
+  //     Alert.alert("Ошибка", "Введите номер телефона.");
+  //     return;
+  //   }
 
-    try {
-      const response = await fetch("http://localhost:5000/sendCode", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ phoneNumber }),
-      });
+  //   try {
+  //     const response = await fetch("http://localhost:5000/sendCode", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({ phoneNumber }),
+  //     });
 
-      const data = await response.json();
+  //     const data = await response.json();
 
-      if (response.ok) {
-        Alert.alert("Код отправлен", "Пожалуйста, проверьте ваш WhatsApp.");
-        navigation.navigate("PhoneConfirmation");
-      } else {
-        Alert.alert("Ошибка", "Не удалось отправить код.");
-      }
-    } catch (error) {
-      console.error(error);
-      Alert.alert("Ошибка", "Что-то пошло не так.");
-    }
-  };
+  //     if (response.ok) {
+  //       Alert.alert("Код отправлен", "Пожалуйста, проверьте ваш WhatsApp.");
+  //       navigation.navigate("PhoneConfirmation");
+  //     } else {
+  //       Alert.alert("Ошибка", "Не удалось отправить код.");
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+  //     Alert.alert("Ошибка", "Что-то пошло не так.");
+  //   }
+  // };
 
   return (
     <Layout isScrollable={false}>
@@ -49,16 +49,17 @@ export const RegistrationScreen = () => {
             {i18n.t("signIn")}
           </Text>
           <View className="w-full items-center mt-20 mb-36">
-            <Input
+            {/* <Input
               labelText={i18n.t("phoneNumber")}
               value={phoneNumber}
               onChangeText={(text) => setPhoneNumber(text)}
-            />
+            /> */}
+            <Input labelText={i18n.t("phoneNumber")} />
           </View>
         </View>
 
         <MyTouchableOpacity
-          onPress={handleSendCode}
+          onPress={() => navigation.navigate("PhoneConfirmation" as never)}
           className="bg-main rounded-[32px] w-64 h-12 flex items-center justify-center"
         >
           <Text weight="bold" className="text-dark text-lg">
